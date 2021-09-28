8a9cbae8614d419a67bd5b460d6fb4ebc543dc06
Jeo <jeo961@gmail.com>
2021-09-28 13:21:16 -0400

    My perfectly working code with no issues

---
+++ Baby Group project/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Baby_Group_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myProjectName = "The cool stor";
+            Console.WriteLine($"Welcome to myProjectName");
+        }
+    }
+}
